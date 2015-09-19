60a8b65ce1f29fa874ad68849c1f2dbdd3e96294
Shang Wang <shangwang@conadmins-MacBook-Air.local>
2015-09-19 13:48:17 -0700

    adding BaiduMap API

---
+++ PayController/PayViewController.h
@@ -0,0 +1,13 @@
+//
+//  PayViewController.h
+//  REFrostedViewControllerStoryboards
+//
+//  Created by Shang Wang on 9/18/15.
+//  Copyright (c) 2015 Shang Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PayViewController : UIViewController
+
+@end
---
+++ PayController/Util.h
@@ -0,0 +1,13 @@
+//
+//  Util.h
+//  REFrostedViewControllerStoryboards
+//
+//  Created by Shang Wang on 9/18/15.
+//  Copyright (c) 2015 Shang Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Util : NSObject
+
+@end
